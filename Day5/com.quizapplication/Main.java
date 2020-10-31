c2ee788ba6d58e664934589a16ac796ef5ae929b
sunnysd2000 <53809871+sunnysd2000@users.noreply.github.com>
2020-11-01 01:29:49 +0530

    Add files via upload

---
+++ Day5/com.quizapplication/Main.java
@@ -0,0 +1,13 @@
+package com.quizapplication;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Quiz application
+        Game game=new Game();
+        game.initGame();
+        game.play();
+
+    }
+}
+
